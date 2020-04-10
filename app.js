3e0b4eec664351aae5bb59d0293406fa6c55c91a
kbpsystem <koleen.bp@outlook.com>
2020-04-11 04:04:14 +0800

    initial try to nodejs&passport authentication

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const errorHandler = require("errorhandler");
+
+mongoose.promise = global.Promise;
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const app = express();
+
+app.use(cors());
+app.use(require("morgan")("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "passport-tutorial",
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+if (!isProduction) {
+  app.use(errorHandler());
+}
+
+// Mongoose
+mongoose.connect("mongodb://localhost/passport-tutorial");
+
+// Error handler && middleware
+if (!isProduction) {
+  app.use((err, req, res) => {
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: err,
+      },
+    });
+  });
+}
+
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+  res.json({
+    errors: {
+      message: err.message,
+      error: {},
+    },
+  });
+});
+
+app.listen(1964, () => console.log(`Server running on http://localhost:1964`));
